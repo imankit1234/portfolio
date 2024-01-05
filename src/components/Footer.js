5e96d0eff3d6f62c607c87e5bdd3b9c4eeadf9bf
Ankit <nks823001@gmail.com>
2024-01-06 05:02:13 +0530

    updates done

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,6 +4,7 @@ import logo from "../assets/img/logo.svg";
 import navIcon1 from "../assets/img/nav-icon1.svg";
 import navIcon2 from "../assets/img/nav-icon2.svg";
 import navIcon3 from "../assets/img/nav-icon3.svg";
+import navIcon7 from "../assets/img/nav-icon7.svg";
 
 export const Footer = () => {
   return (
@@ -16,9 +17,10 @@ export const Footer = () => {
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
-              <a href="#"><img src={navIcon1} alt="Icon" /></a>
-              <a href="#"><img src={navIcon2} alt="Icon" /></a>
-              <a href="#"><img src={navIcon3} alt="Icon" /></a>
+            <a href="https://www.linkedin.com/in/imankit81/"><img src={navIcon1} alt="" /></a>
+            <a href="https://github.com/imankit1234"><img src={navIcon2} alt="" /></a>
+            <a href="https://twitter.com/imankit81"><img src={navIcon3} alt="" /></a>
+              <a href="nks823001@gmail.com"><img src={navIcon7} alt="Icon" /></a>
             </div>
             <p>Copyright 2022. All Rights Reserved</p>
           </Col>
